dafa98534015f66ea147b83f806871051545c593
Przemek <wozniak.przem@gmail.com>
2020-03-23 21:38:33 +0100

    Adapter implemented

---
+++ adapter.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class Printer
+{
+public:
+	virtual void print() const;
+};
+
+class StringPrinter : public Printer
+{
+public:
+	StringPrinter(std::string str);
+	void print() const override;
+private:
+	std::string s;
+};
+
+
+int main()
+{
+	StringPrinter sp("hello");
+	sp.print();
+}
+
+void Printer::print() const
+{
+	std::puts("yeoyooye");
+}
+
+StringPrinter::StringPrinter(std::string str) : s(std::move(str))
+{
+
+}
+
+void StringPrinter::print() const
+{
+	std::for_each(s.begin(), s.end(), [](auto & letter){ std::cout << letter << '\n'; });
+}
